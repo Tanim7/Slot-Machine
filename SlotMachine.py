4ee77a0f20fbe189d807707d60e87360d6fef1f7
Tanim7 <101062172+Tanim7@users.noreply.github.com>
2022-10-14 00:58:35 -0400

    Update SlotMachine.py

--- SlotMachine.py
+++ SlotMachine.py
@@ -38,7 +38,7 @@ def check_winnings(columns, lines, bet, values): #Calculate the total winnings.
     return winnings, winning_lines
 
 
-def get_slot_machine_spin(rows, cols, symbols):
+def get_slot_machine_spin(rows, cols, symbols):  #
     all_symbols = []
     for symbol, symbol_count in symbols.items():
         for _ in range(symbol_count):
